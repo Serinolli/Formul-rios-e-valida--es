83876c925e28e788e5f2f64b9d823897b3231385
Serinolli <rafael.serinolli2@gmail.com>
2020-05-18 22:39:13 -0300

    Resolvida tela preta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   },
   {
     path: 'cpf-validator',
-    loadChildren: () => import('./cpf-validator/cpf-validator.module').then( m => m.CpfValdiatorPageModule)
+    loadChildren: () => import('./Validadores/cpf-validator').then( m => m.CpfValidator)
   },
 ];
 
